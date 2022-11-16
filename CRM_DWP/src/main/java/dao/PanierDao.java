8d94cfec3f159392a36202d7c9b55241cb6e9901
Alexandra Cantarano <alexandra.cantarano@gmail.com>
2022-11-16 14:15:40 +0100

    add PanierDao and PanierDaoImpl

---
+++ CRM_DWP/src/main/java/dao/PanierDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.util.List;
+
+import model.Panier;
+
+public interface PanierDao {
+	
+	void         creer( Panier panier ) throws DaoException;
+
+    Panier       trouver( long id ) throws DaoException;
+
+    List<Panier> lister() throws DaoException;
+
+    void         supprimer( long id ) throws DaoException;
+
+    void         update( Panier panier ) throws DaoException;
+	
+	
+}
