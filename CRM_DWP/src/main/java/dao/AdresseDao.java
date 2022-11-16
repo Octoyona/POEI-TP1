cc9a82c2051823ca33f5c0053556ea8967d5a6a5
Alexandra Cantarano <alexandra.cantarano@gmail.com>
2022-11-16 15:56:27 +0100

    Add DAO Adresse

---
+++ CRM_DWP/src/main/java/dao/AdresseDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+import model.Adresse;
+
+public interface AdresseDao {
+	
+	void         creer( Adresse adresse ) throws DaoException;
+
+	Adresse       trouver( long id ) throws DaoException;
+
+    List<Adresse> lister() throws DaoException;
+
+    void         supprimer( long id ) throws DaoException;
+
+    void         update( Adresse adresse ) throws DaoException;
+}
