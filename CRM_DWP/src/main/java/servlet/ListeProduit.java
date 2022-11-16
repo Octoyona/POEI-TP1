585e8ec7527ca065301f3592f84630c1bfbcc3b6
Antoine Aribard <aaribard@gmail.com>
2022-11-16 15:21:32 +0100

    liste produit chagement nom

--- CRM_DWP/src/main/java/servlet/ListeProduit.java
+++ CRM_DWP/src/main/java/servlet/ListeProduit.java
@@ -13,24 +13,15 @@ import dao.PanierDao;
 import dao.ProduitDao;
 import model.Contient;
 import model.Produit;
-/**
- * Servlet implementation class ChoixProduit
- */
-@WebServlet("/choixProduit")
-public class ChoixProduit extends HttpServlet {
+
+@WebServlet("/listeProduit")
+public class ListeProduit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ChoixProduit() {
+    public ListeProduit() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		ProduitDao produitsDao = DaoFactory.getInstance().getProduitDao();
@@ -47,9 +38,6 @@ public class ChoixProduit extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
